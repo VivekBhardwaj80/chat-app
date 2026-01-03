ecb49b43f4fdcc454429312b566632f6c2baa8c6
VivekBhardwaj80 <vp0444199@gmail.com>
2026-01-04 02:30:35 +0530

    Update cookie settings for token in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,8 +31,8 @@ try {
     res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"Strict",
-        secure:false
+        sameSite:"None",
+        secure:true
     })
 
     res.status(201).json({success:true,message:"User Created",user})
@@ -64,8 +64,8 @@ try {
     res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"Strict",
-        secure:false
+        sameSite:"None",
+        secure:true
     })
 
     res.status(200).json({success:true,message:"User successfully Login",user})
