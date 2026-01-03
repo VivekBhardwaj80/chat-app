005b18ef395d8e7647d10d5ac423974ca8051546
VivekBhardwaj80 <vp0444199@gmail.com>
2026-01-04 02:28:08 +0530

    Update CORS origin for socket server

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:process.env.CLIENT_URL,
+        origin:"https://chat-app-lh2w.onrender.com",
     }
 })
 
